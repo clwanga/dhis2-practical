fd1c209ca972a9c3630ae9ef5e03f7d303a9fcc8
Charles Lwanga <clwanga1095@gmail.com>
2024-01-26 20:23:17 +0300

    Integrated with jokes api, and added the view

--- src/app/Services/apiRequests.service.ts
+++ src/app/Services/apiRequests.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { WeatherResponse } from "../Models/WeatherResponse";
+import { JokesResponse } from "../Models/JokesResponse";
 
 @Injectable({
     providedIn: 'root'
@@ -9,11 +10,18 @@ import { WeatherResponse } from "../Models/WeatherResponse";
 export class APIRequest{
     public apiKey:string  = "70c8b3004ccba1ac189e8d627581ced1";
     baseUrl:string = "https://api.openweathermap.org";
+    baseUrlJokesApi:string = "https://official-joke-api.appspot.com";
 
     constructor(private http: HttpClient){} 
 
     getWeatherDetails(cityName:string): Observable<WeatherResponse>{
       const url = `${this.baseUrl}/data/2.5/weather?q=${cityName}&appid=${this.apiKey}`;
       return this.http.get<WeatherResponse>(url);
+    }
+
+    getRandomJoke(): Observable<JokesResponse>{
+      const endpoint = `${this.baseUrlJokesApi}/random_joke`;
+
+      return this.http.get<JokesResponse>(endpoint);
     }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HotToastService } from '@ngneat/hot-toast';
 import { APIRequest } from '../Services/apiRequests.service';
 import { WeatherResponse } from '../Models/WeatherResponse';
+import { JokesResponse } from '../Models/JokesResponse';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent  implements OnInit{
     //properties
   weatherDetails: WeatherResponse;
+  jokeApiResponse: JokesResponse;
+  showJokes: boolean = true;
 
+  //constructor
   constructor(private api:APIRequest, private toast:HotToastService){};
+
+  //methods
+  ngOnInit(){
+    this.api.getRandomJoke().subscribe({
+      next:(response: JokesResponse)=>{
+        this.jokeApiResponse = response;
+      },
+      error(){
+        this.toast.create('Error fetching Joke!','danger');
+      }
+    });
+  }
   
   onButtonClick(name:string){
+    this.showJokes =false;
+
     this.api.getWeatherDetails(name).pipe(this.toast.observe({
       loading: 'Loading details..',
       success: 'Done!',
